f47473fc934a7ad72889efdbf7a28282bec7e30f
Fernando Almendro Martí <72808136+FernaToty@users.noreply.github.com>
2022-11-24 09:54:38 +0100

    wqiorwj+rwr9'r29u'r2'u9

---
+++ Brenda 6 The end of an era/Assets/Scripts/Target.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public bool Activat = false;
+
+    public void Activar()
+    {
+        Debug.Log("Activat!");
+        Activat = true;
+    }
+
+
+    public void Rebre()
+    {
+        Debug.Log("Rebut!");
+    }
+
+}
