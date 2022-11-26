1aa78cee1a7ba10ab7a5fc90adbd41be8cc80484
Fernando Almendro Martí <72808136+FernaToty@users.noreply.github.com>
2022-11-26 17:31:55 +0100

    vamoooooooooo

---
+++ artificialInteligence/Assets/Behaviur trees/Rebut.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using Pada1.BBCore;
+using Pada1.BBCore.Framework;
+
+[Condition("MyConditions/Rebut?")]
+[Help("Checks bool Valor.")]
+public class Rebut : ConditionBase
+{
+    [InParam("game object")]
+    [Help("Game object to add the component, if no assigned the component is added to the game object of this behavior")]
+    public GameObject go;
+
+    public override bool Check()
+    {
+        Target tgt = go.GetComponent<Target>();
+        return tgt.Activat;
+    }
+}
