6cf63fad780c3061907fc5d7c11a1bb5f1014245
Fernando Almendro Martí <72808136+FernaToty@users.noreply.github.com>
2023-01-14 18:35:59 +0100

    Tai chi done

---
+++ artificialInteligence/Assets/Scipts/Entrega4/Formasion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Formasion : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject target;
+    public Vector3 pos;
+    public Quaternion rot;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Leader");
+        transform.rotation = target.transform.rotation;
+        transform.position = target.transform.TransformPoint(pos);
+    }
+
+    void Update()
+    {
+        agent.destination = target.transform.TransformPoint(pos);
+    }
+}
---
+++ artificialInteligence/Assets/Scipts/Entrega4/Slouts.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Slouts : MonoBehaviour
+{
+    public int melee;
+    public GameObject meleePrefab;
+    public int missile;
+    public GameObject missilePrefab;
+    public int magic;
+    public GameObject magicPrefab;
+    public GameObject ghost;
+
+    void Start()
+    {
+        int front = 2 * melee / 3;
+        int rear = melee - front;
+        createRow(front, -2f, meleePrefab);
+        createRow(missile, -4f, missilePrefab);
+        createRow(magic, -6f, magicPrefab);
+        createRow(rear, -8f, meleePrefab);
+    }
+
+    void createRow(int num, float z, GameObject pf)
+    {
+        float pos = 1 - num;
+        for (int i = 0; i < num; ++i)
+        {
+            Vector3 position = ghost.transform.TransformPoint(new Vector3(pos, 0f, z));
+            GameObject temp = (GameObject)Instantiate(pf, position, ghost.transform.rotation);
+            
+            pos += 2f;
+        }
+    }
+}
